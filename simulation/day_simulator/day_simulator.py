6d886124df85a98788e90c870f3e1a63e2bb1f17
Josiah Cilladi <josiahcilladi@gmail.com>
2022-12-26 19:47:55 -0500

    broke out endpoints to lightspeeds

--- simulation/day_simulator/day_simulator.py
+++ simulation/day_simulator/day_simulator.py
@@ -3,23 +3,24 @@
 from datetime import datetime
 from datetime import timedelta
 from time import sleep
-
+import day_simulator.endpoints as endpoints
 
 # Receve Data from Simulation Config Slack Modle
 def get_day_simulation_config(view):
-    # print(json.dumps(view, indent =1))
    
     time_span = view["time_span"]["time_span"]["value"]
     num_shifts = view["number_of_shifts"]["number_of_shifts"]["value"]
-    items_sold = view["items_sold"]["items_sold"]["value"]
+    num_total_sales = view["num_total_sales"]["num_total_sales"]["value"]
     num_ecom = view["number_ecom_orders"]["number_ecom_orders"]["value"]
     # print(store_open,store_close,num_shifts,total_sales,num_ecom)
-    start_day_simulation(time_span, num_shifts, items_sold, num_ecom)
-
+    start_day_simulation(time_span, num_shifts, num_total_sales, num_ecom)
+    return
     
-def start_day_simulation(time_span, num_shifts, items_sold, num_ecom):
-    print(time_span, num_shifts, items_sold, num_ecom)
-    items_sold = int(items_sold)
+
+# start the day simlator
+def start_day_simulation(time_span, num_shifts, num_total_sales, num_ecom):
+    print(time_span, num_shifts, num_total_sales, num_ecom)
+    num_total_sales = int(num_total_sales)
     num_shifts = int(num_shifts)
     num_ecom = int(num_ecom)
     # Add tiem_span to current time
@@ -30,48 +31,45 @@ def start_day_simulation(time_span, num_shifts, items_sold, num_ecom):
     time_span_end = today + timedelta(minutes=delta_seconds)
     print("time_span_end", time_span_end)
 
-    x = items_sold/num_shifts
+    x = num_total_sales/num_shifts
     print("shift duration by Items sold.", x)
     y = x
-
-    p = round(items_sold/num_ecom)
+    p = round(num_total_sales/num_ecom)
     q = p
-
-
-
-    for i in range(0,items_sold + 1):
+    for i in range(0, num_total_sales + 1):
         if i == 1:
-            print("Store Opens, Shift",1, "Starts")
+            print("Store Opens, Shift",1, "Starts *******************")
+            # Open Register
+            endpoints.open_register()
 
-        print("sold Item",i)
-        sleep(delta_seconds/items_sold)
+        print("sale num",i)
+        # Create Sale
+        endpoints.create_sale()
 
-      
+        sleep(delta_seconds/num_total_sales)
         shift_num = round(i/x)
-        print("shift_num", shift_num)
-
         if i >= round(q):
             q = q + p
             print("eCom Order", round((q/p)-1), "xxxxxxxxx")
+            # Create eCom Sale
+            endpoints.create_ecom_sale()
 
-
-
-        # print("y", y)
         if i >= round(y):
-            
             y = y + x
-            
             print("Shift", shift_num , "Ends --------")
+            # Close Register
+            endpoints.close_register()
 
-            if i == items_sold:
+            if i == num_total_sales:
                 pass
-                print("End of Day!")
+                print("End of Day! **********************")
             else:
                 print("Shift", shift_num +1 ,"Starts vvvvvvv")
-            
+                # Open Register
+                endpoints.open_register()
         
 
-    pass
+    return
    
     
 
---
+++ simulation/day_simulator/endpoints.py
@@ -0,0 +1,20 @@
+# Files
+# from services.lightspeed.lightspeed_r import oauth
+
+def open_register():
+    print("open_register")
+    pass
+
+def close_register():
+    print("close_register")
+    pass
+
+def create_sale():
+    print("create_sale")
+    pass
+
+def create_ecom_sale():
+    print("create_ecom_sale")
+    pass
+
+
